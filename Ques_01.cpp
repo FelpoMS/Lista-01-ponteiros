5ee9543b4dd3e15ef48fdb76d474007e7f95e8dd
Felpo <160980435+FelpoMS@users.noreply.github.com>
2025-03-12 20:37:54 -0300

    quest trabalho

---
+++ Ques_01.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//1. Cadastro de softwares para uma loja de Aplicativos.
+struct Software{
+	char nom;
+	char cat; // produtividade, jogos , saude , edc...
+	int notn; // de 1 - 10.
+};
+
+
+void mednota(){	
+}
+
+int main(){
+	char nome,categoria;
+	int nota,x;
+	
+	
+	printf("quantos softwares pretende adicionar?:");
+	scanf("%d",&x);
+	
+	for(int i=0;i<=x-1;i++){
+	Software s[i];
+		
+	printf("Cadastro de Software. ()nome: \n");
+	scanf("%c",s[i].nom);
+	
+	printf("\nCadastro de Software. ()categoria: \n");
+	scanf("%c",s[i].cat);
+	
+	printf("\nCadastro de Software. ()nota(1-10): \n");
+	scanf("%d",&s[i].notn);
+	
+	}
+	
+	
+	
+	
+}
+

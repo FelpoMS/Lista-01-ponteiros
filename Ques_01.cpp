319483e659770f5826330ffb01868403ef8117be
Felpo <160980435+FelpoMS@users.noreply.github.com>
2025-03-12 21:44:34 -0300

    Update Ques_01.cpp
    ideia estruturada.

--- Ques_01.cpp
+++ Ques_01.cpp
@@ -2,16 +2,48 @@
 #include <stdio.h>
 
 //1. Cadastro de softwares para uma loja de Aplicativos.
+
 struct Software{
 	char nom;
 	char cat; // produtividade, jogos , saude , edc...
 	int notn; // de 1 - 10.
 };
-
-
-void mednota(){	
+///////////////////////////////////////////////////////////////////////////////////////////////
+void listar(char a;char b; int c){// printar os softwares
+	char nom;//nome do software
+	char clas;// classe.
+	int no;//nota
+	
+  printf("Software: %s, classe: %s, nota: %d",nom,clas,no);
+	
+}	
+	
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////
+void maiornota(char a,int b,int x){ //verificar maior nota
+	
+int maior;//nota maior media
+int s; //nota do software
+int x; //contador de software
+
+for(int i=0;i<=x-1;i++){
+  
+  if (s[i]>s[i+1]){
+  	maior=s[i];
+  	i++;
+  }else {
+  	i++;
+  }
+  
+	
+}
+
+ return maior;
+}
+
+
+///////////////////////////////////////////////--MAIN--///////////////////////////////////////////
 int main(){
 	char nome,categoria;
 	int nota,x;
@@ -24,10 +56,10 @@ int main(){
 	Software s[i];
 		
 	printf("Cadastro de Software. ()nome: \n");
-	scanf("%s",s[i].nom);
+	scanf("%s",&s[i].nom);
 	
 	printf("\nCadastro de Software. ()categoria: \n");
-	scanf("%s",s[i].cat);
+	scanf("%s",&s[i].cat);
 	
 	printf("\nCadastro de Software. ()nota(1-10): \n");
 	scanf("%d",&s[i].notn);
@@ -38,4 +70,3 @@ int main(){
 	
 	
 }
-
